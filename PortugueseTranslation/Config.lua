5d98a656f282a298cb019b723c65fbc76ea3e26d
Yuri <isyuricunha@duck.com>
2024-08-08 07:19:54 -0300

    Initial commit

---
+++ PortugueseTranslation/Config.lua
@@ -0,0 +1,32 @@
+return {
+	Title = "[PT-BR] Tradução Português/Portugues Brasileiro",
+	Author = "Yuri",
+	Description = "Scroll of Taiwu - Português Brasileiro",
+	HasArchive = false,
+	FrontendPlugins = {
+		[1] = "TaiwuCommunityTranslation.Frontend.dll",
+	},
+	BackendPlugins = {
+		[1] = "TaiwuCommunityTranslation.Backend.dll",
+	},
+	Source = 1,
+	FileId = 3305069082,
+	Version = "0.0.71.78",
+	GameVersion = "0.0.72.14",
+	Visibility = 0,
+	DefaultSettings = { },
+	UpdateLogList = {
+		[1] = {
+			Timestamp = 1723110895,
+		},
+	},
+	ChangeConfig = false,
+	NeedRestartWhenSettingChanged = true,
+	Cover = "vai-brasil.png",
+	WorkshopCover = "vai-brasil.png",
+	TagList = {
+		[1] = "Configurations",
+		[2] = "Extensions",
+		[3] = "Display",
+	},
+}
---
+++ PortugueseTranslation/V2/PortugueseTranslation/Mod/CommunityTranslation/Config.lua
@@ -0,0 +1,21 @@
+return {
+	Title = "Taiwu Traducao Portugues Brasileiro",
+	Author = "Yuri",
+	Description = "Taiwu Traducao Portugues Brasileiro.",
+	HasArchive = false,
+	FrontendPlugins = {
+		[1] = "TaiwuCommunityTranslation.Frontend.dll",
+	},
+	BackendPlugins = {
+		[1] = "TaiwuCommunityTranslation.Backend.dll",
+	},
+	Source = 0,
+	FileId = 200,
+	Version = "0.0.71.78",
+	GameVersion = "0.0.71.78",
+	Visibility = 0,
+	DefaultSettings = { },
+	UpdateLogList = { },
+	Cover = nil,
+	WorkshopCover = nil,
+}
