9960374e0c87c130028d496b43ad6c60c5c88c95
Yuri <isyuricunha@duck.com>
2024-08-08 17:36:16 -0300

    remake

--- PortugueseTranslation/Config.lua
+++ PortugueseTranslation/Config.lua
@@ -1,7 +1,7 @@
 return {
 	Title = "[PT-BR] Tradução Português/Portugues Brasileiro",
 	Author = "Yuri",
-	Description = "Scroll of Taiwu - Português Brasileiro",
+	Description = "[h1][PT-BR] Tradução Português/Português Brasileiro[/h1]\r\n\r\n[b]Status:[/b] Em desenvolvimento\r\n\r\n[b]Ajude a manter o projeto via Pix:[/b] pix@yuricunha.com\r\n\r\n[b]Participe da comunidade no Discord:[/b] [url=https://discord.com/invite/aYbWBr4]Clique aqui[/url]\r\n\r\n[hr][/hr]\r\n\r\nOlá,\r\n\r\nEste é o mod de tradução para o jogo [i]Scroll Of Taiwu[/i]. Aqui estão algumas informações importantes:\r\n\r\n- [b]Doações:[/b] Se você gosta do meu trabalho, considere fazer uma doação para me apoiar.\r\n- [b]Caracteres Especiais:[/b] Devido a limitações do jogo, alguns caracteres especiais como \"ç\", \"ã\" e \"ô\" foram substituídos. Por exemplo, \"coração\" aparecerá como \"coracao\".\r\n- [b]Progresso da Tradução:[/b] Atualmente, a tradução está 55% completa. Apesar disso, você já poderá entender grande parte do jogo.\r\n- [b]Fontes de Tradução:[/b] Estou traduzindo diretamente do chinês e japonês, mantendo a fidelidade às referências originais. As partes ainda não traduzidas estão em inglês para facilitar a compreensão.\r\n- [b]Atualizações:[/b] O mod será atualizado duas vezes por semana até que a tradução esteja completa. Vale ressaltar que estou constantemente corrigindo e traduzindo novas partes, então você pode esperar melhorias contínuas.\r\n- [b]Meta:[/b] Espero concluir a tradução ainda este mês, faltando apenas a revisão final.\r\n\r\nAproveite o mod e obrigado pelo seu apoio!\r\n\r\nSe cuidem,\r\nYuri\r\n\r\n[hr][/hr]\r\n\r\nLink para a versão na Steam: https://steamcommunity.com/sharedfiles/filedetails/?id=3305069082\r\n\r\nLink para versões alternativas (Steam verde): https://github.com/isyuricunha/scroll-of-taiwu-traducao-portugues-brasileiro/releases/latest\r\n\r\n[hr][/hr]\r\n\r\n# Como instalar:\r\n\r\n## Versão Steam:\r\n\r\n- Inscreva-se no link da oficina.\r\n- Abra o jogo. Clique no último ícone colorido no canto inferior direito da tela principal, o que abrirá outra tela com o nome do mod \"[PT-BR] Tradução...\". Clique na caixa ao lado esquerdo, abaixo do nome do mod, e depois no botão azul no canto inferior esquerdo. Em seguida, clique no botão à esquerda do menu que aparecerá. O jogo irá reiniciar, mas caso isso não aconteça em cerca de 60 segundos, abra-o manualmente, e ele começará com a tradução já aplicada.\r\n\r\n## Versão alternativa (Steam verde):\r\n\r\n- Baixe o arquivo no link indicado anteriormente. Abra a pasta raiz do jogo (isso varia de computador para computador). Se você não souber onde o jogo está instalado, clique com o botão direito no ícone/atalho do jogo e localize a opção \"Abrir localização do arquivo\" ou algo semelhante.\r\n- Procure a pasta chamada \"Mods\" e extraia o arquivo/zip \"PortugueseTranslation.zip\" dentro dessa pasta.\r\n- Abra o jogo. Clique no último ícone colorido no canto inferior direito da tela principal, o que abrirá outra tela com o nome do mod \"[PT-BR] Tradução...\". Clique na caixa ao lado esquerdo, abaixo do nome do mod, e depois no botão azul no canto inferior esquerdo. Em seguida, clique no botão à esquerda do menu que aparecerá. O jogo irá reiniciar, mas caso isso não aconteça em cerca de 60 segundos, abra-o manualmente, e ele começará com a tradução já aplicada.\r\n",
 	HasArchive = false,
 	FrontendPlugins = {
 		[1] = "TaiwuCommunityTranslation.Frontend.dll",
@@ -10,7 +10,7 @@ return {
 		[1] = "TaiwuCommunityTranslation.Backend.dll",
 	},
 	Source = 1,
-	FileId = 3305069082,
+	FileId = 3305308095,
 	Version = "0.0.71.78",
 	GameVersion = "0.0.72.14",
 	Visibility = 0,
@@ -19,9 +19,18 @@ return {
 		[1] = {
 			Timestamp = 1723110895,
 		},
+		[2] = {
+			Timestamp = 1723122968,
+			LogList = {
+				[1] = "- Atualizado algumas palavras que ficavam bugadas, devido ao jogo nao suportar UTF8",
+			},
+		},
+		[3] = {
+			Timestamp = 1723148198,
+		},
 	},
 	ChangeConfig = false,
-	NeedRestartWhenSettingChanged = true,
+	NeedRestartWhenSettingChanged = false,
 	Cover = "vai-brasil.png",
 	WorkshopCover = "vai-brasil.png",
 	TagList = {
