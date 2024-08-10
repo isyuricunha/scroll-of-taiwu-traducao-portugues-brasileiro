b80581dca2fe54eb84fbf9d37ce263c9c9756cdf
Yuri <isyuricunha@duck.com>
2024-08-10 12:19:33 -0300

    Update Config.lua

--- PortugueseTranslation/Config.lua
+++ PortugueseTranslation/Config.lua
@@ -43,6 +43,12 @@ return {
 				[1] = "Commits on Aug 9, 2024\r\nUpdate PortugueseTranslation.zip\r\n\r\n@isyuricunha\r\nisyuricunha committed yesterday\r\ncolocando arquivos ingles \r\n\r\n@isyuricunha\r\nisyuricunha committed yesterday\r\nUpdate Config.lua\r\n\r\n@isyuricunha\r\nisyuricunha committed yesterday\r\nRevert \"colocando arquivos ingles\" \r\n\r\n@isyuricunha\r\nisyuricunha committed yesterday\r\nUpdate events.json\r\n\r\n@isyuricunha\r\nisyuricunha committed 13 hours ago\r\nCommits on Aug 10, 2024\r\nnovas traducoes\r\n\r\n@isyuricunha\r\nisyuricunha committed 1 hour ago\r\nremovido \"ç\", \"ã\" e \"õ\" da nova versao \r\n\r\nremovido \"ç\", \"ã\" e \"õ\"\r\n@isyuricunha\r\nisyuricunha committed 1 hour ago\r\narrumado palavras avulsas e string color \r\n\r\nigual -> neutro\r\n<cor# -> <#color\r\n@isyuricunha\r\nisyuricunha committed 1 hour ago\r\nfix: diversas variaveis\r\n\r\n@isyuricunha\r\nisyuricunha committed 14 minutes ago\r\nUpdate PortugueseTranslation.zip\r\n\r\n@isyuricunha\r\nisyuricunha committed 13 minutes ago",
 			},
 		},
+		[7] = {
+			Timestamp = 1723302530,
+			LogList = {
+				[1] = "Adventure_language\r\nAccessory_language\r\nMisc_language\r\nLoadingTips_language",
+			},
+		},
 	},
 	ChangeConfig = false,
 	NeedRestartWhenSettingChanged = false,
